7c311e4dba21d0c90d9add88c827361955027c24
ivial <phunh15012004@gmail.com>
2025-05-20 16:28:44 +0700

    test data user v4

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { getLocgetlStorage } from "../utils/util";
 
 const initialState = {
-  infoUser: getLocgetlStorage("user"),
+  infoUser: JSON.parse(getLocgetlStorage("user")),
 };
 
 const authSlice = createSlice({
