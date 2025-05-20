809fe4b55467dbbccf9ad1fae87a9cb90752da7d
ivial <phunh15012004@gmail.com>
2025-05-20 15:54:04 +0700

    test data user

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { path } from "../common/path";
+import { useSelector } from "react-redux";
+import { Avatar } from "antd";
 
 const UserHeader = () => {
+  const { infoUser } = useSelector((state) => state.authSlice);
+
+  const checUserkLogin = () => {
+    return infoUser ? (
+      <Avatar>{infoUser.fullname}</Avatar>
+    ) : (
+      <>
+        {" "}
+        <Link
+          to={path.signIn}
+          className="py-2 px-4 border border-red-500 rounded-md hover:bg-red-500 duration-300"
+        >
+          sign in
+        </Link>
+        <Link
+          to={path.signUp}
+          className="py-2 px-4 border border-green-500 rounded-md hover:bg-green-600 duration-300"
+        >
+          sign up
+        </Link>
+      </>
+    );
+  };
+
   return (
     <header>
       <div className="container">
