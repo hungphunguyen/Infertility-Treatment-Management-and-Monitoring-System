6287ab983bc8ca8d671bdb8afe85c31368054b7e
ivial <phunh15012004@gmail.com>
2025-05-20 16:52:27 +0700

    fix save v5

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -12,7 +12,6 @@ const UserHeader = () => {
       <Avatar>{infoUser.fullName}</Avatar>
     ) : (
       <>
-        {" "}
         <Link
           to={path.signIn}
           className="py-2 px-4 border border-red-500 rounded-md hover:bg-red-500 duration-300"
@@ -33,22 +32,9 @@ const UserHeader = () => {
     <header>
       <div className="container">
         <div className="header_logo ">
-          <Link to={path.homePage}>Logo 00here</Link>
+          <Link to={path.homePage}>Logo here</Link>
         </div>
-        <nav className="header_navigate">
-          <Link
-            to={path.signIn}
-            className="py-2 px-4 border border-red-500 rounded-md hover:bg-red-500 duration-300"
-          >
-            sign in
-          </Link>
-          <Link
-            to={path.signUp}
-            className="py-2 px-4 border border-green-500 rounded-md hover:bg-green-600 duration-300"
-          >
-            sign up
-          </Link>
-        </nav>
+        <nav className="header_navigate">{checUserkLogin()}</nav>
       </div>
     </header>
   );
