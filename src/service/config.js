6ef9f8e29ce465ac336f868400fa972845a8f26d
Lam Nguyen <lamnpse184388@fpt.edu.vn>
2025-05-30 15:12:36 +0700

    Api doctor V2

--- src/service/config.js
+++ src/service/config.js
@@ -1,40 +1,10 @@
 import axios from "axios";
-import { getLocgetlStorage } from "../utils/util";
 
-// Setup axios cho dự án
+//setup axios custom xử lí gọi API cho dự án
 const http = axios.create({
-  baseURL: "http://www.infertilitymonitoring.unaux.com/infertility-system-api/",
+  baseURL: "http://www.infertilitymonitoring.unaux.com/infertility-system-api/", // domain
   timeout: 30000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  headers: {},
 });
-
-// Request interceptor - thêm token nếu có
-http.interceptors.request.use(
-  (config) => {
-    const token = getLocgetlStorage("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-// Response interceptor - xử lý lỗi
-http.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error("API Error:", {
-      status: error.response?.status,
-      message: error.response?.data?.message || error.message,
-      url: error.config?.url
-    });
-    return Promise.reject(error);
-  }
-);
 
 export { http };
