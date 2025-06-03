ac2c6cb5c023d9c296b2395f76e2b3482841e8c2
ivial <phunh15012004@gmail.com>
2025-06-03 12:21:59 +0700

    change domain

--- src/service/config.js
+++ src/service/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //setup axios custom xử lí gọi API cho dự án
 const http = axios.create({
-  baseURL: "http://www.infertilitymonitoring.unaux.com/infertility-system-api/", // domain
+  baseURL: "http://35.78.68.64/infertility-system-api/", // domain
   timeout: 30000,
   headers: {},
 });
